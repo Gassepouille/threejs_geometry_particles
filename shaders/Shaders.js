ab505db9dddf7cc168d331e30e8c680b7b3c3da8
Gaspard Guillaume <gaspard.guillaume@daqri.com>
2017-02-23 16:53:15 +0000

    clean shader file

--- shaders/Shaders.js
+++ shaders/Shaders.js
@@ -9,8 +9,7 @@ APP.Shaders = class Shaders {
 			vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
 			gl_PointSize = amplitude *  -mvPosition.z ;
 			gl_Position = projectionMatrix * mvPosition;
-		}
-		`;
+		}`;
 	}
 	static getParticlesFragment(){
 		return `
@@ -20,7 +19,6 @@ APP.Shaders = class Shaders {
 		void main() {
 			gl_FragColor = vec4( color, 1.0 );
 			gl_FragColor = gl_FragColor * texture2D( texture, gl_PointCoord );;
-		}
-		`;
+		}`;
 	}
 }
